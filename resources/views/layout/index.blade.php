14ba2079977a7d78f8365279369a91a37eb30440
airton-silva <airtonsilvan89@gmail.com>
2024-06-15 17:29:02 -0300

    problema relacionado ao link de chamada do js

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -4,8 +4,8 @@
 <meta charset="utf-8">
 	<title>show do milhão mjailton</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<script type="text/javascript" src="{{assets(assets/js/jquery-3.1.1.min.js)}}"></script> 
-	<script type="text/javascript" src="{{assets(assets/js/js.js)}}"></script> 
+	<script type="text/javascript" src="{{asset('assets/js/jquery-3.1.1.min.js')}}"></script> 
+	<script type="text/javascript" src="{{asset('assets/js/js.js')}}"></script> 
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
 </head>
 <body>
