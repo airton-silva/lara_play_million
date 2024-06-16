364fddf1cce1f9928e62ad3fe0a339d28d9c388d
airton-silva <airtonsilvan89@gmail.com>
2024-06-16 16:19:47 -0300

    ciar migrate jogo

---
+++ database/migrations/2024_06_15_212042_create_games_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('level_id');
+            $table->String('value_gain');
+            $table->String('finished');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade');
+
+            $table->foreign('level_id')
+            ->references('id')
+            ->on('levels')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
