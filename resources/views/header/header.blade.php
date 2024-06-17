ee80038859e7940e5ed23739721f2cd952adce0c
airton-silva <airtonsilvan89@gmail.com>
2024-06-17 15:00:38 -0300

    rota de logout

--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -2,7 +2,7 @@
     <div class="conteudo">
         <div class="base-botoes-topo">			
             <a href="" class="btn estatistica">Estatistica</a>	
-            <a href="" class="btn sair">sair</a>
+            <a href="{{url('logout')}}" class="btn sair">sair</a>
         </div>
         <span class="logo"></span>
         <div class="usuario">
