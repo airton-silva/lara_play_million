531447fb4248c2b96e11ece0617575e89ac44897
airton-silva <airtonsilvan89@gmail.com>
2024-06-17 14:58:46 -0300

    controller Login metodos autenticat e logout

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+        /**
+     * Handle an authentication attempt.
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        $user = User::where('email', '=' , $credentials['email'])->where('password', '=' , $credentials['password'])->get();
+ 
+        if ($user){
+            $request->session()->regenerate();
+            return view('home.index');
+        }
+        // dd(Auth::login($user));
+        // if (Auth::attempt($credentials)) {
+        //     $request->session()->regenerate();
+ 
+        //     return redirect()->intended('home.index');
+        // }
+ 
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+    
+        $request->session()->invalidate();
+    
+        $request->session()->regenerateToken();
+    
+        return redirect('/');
+    }
+}
