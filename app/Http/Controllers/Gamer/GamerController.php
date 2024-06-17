1c5a0686d9faf08285b58c0b6d5cdc3d134b1a5e
airton-silva <airtonsilvan89@gmail.com>
2024-06-17 14:59:12 -0300

    controllerGamer medodo index

---
+++ app/Http/Controllers/Gamer/GamerController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Gamer;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GamerController extends Controller
+{
+    function index() {
+        return view('home.index');
+        
+    }
+}
