d45012db8d5516618e8b410284fb84817a174f01
airton-silva <airtonsilvan89@gmail.com>
2024-06-18 15:40:52 -0300

    visualização de sessão flesh erros e adição de rotas de login e register

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -2,62 +2,71 @@
 @section('title', 'Sign')
 
 @section('content')
-    
-<div class="base-topo">
-    <div class="conteudo">
-        <span class="logo"></span>
-    </div>
-</div>
 
-<div class="conteudo">
-<div class="base-inicial">
-    <div class="base-logar">
-        <form action="{{url('/auth')}}" method="POST">
-            @csrf
-            <div class="col">
-                <p class="ttl">Logar</p>
-                <small>Entre com seu login e senha</small>
-                <label>
-                    <span>Usuario/ email</span>
-                    <input type="text" name="email" placeholder="Digite seu nome de usuário">
-                </label>
-                <label>
-                    <span>Senha</span>
-                    <input type="text" name="password" placeholder="Digite sua senha">	
-                </label>
-                <label><a href="#janela1" rel="modal">Esqueci minha senha</a></label>
-                
-                <input type="submit" value="entrar" class="btn">
+    <div class="conteudo">
+        <div class="base-inicial">
+            <div>
+                <!-- Session Status -->
+                <x-auth-session-status class="mb-4" :status="session('status')" />
+
+                <!-- Validation Errors -->
+                <x-auth-validation-errors class="mb-4" :errors="$errors" />
             </div>
-            
-            
-            <div class="col2">
-                
-                <p class="ttl">cadastre-se</p>
-                <small>Se você ainda não é cadastrado, cadastre-se abaixo</small>
-                
-                <label>
-                <span>email</span>
-                <input type="text" name="" placeholder="Digite um email válido">
-                </label>
-                
-                <label>
-                <span>Nome /usuário</span>
-                <input type="text" name="" placeholder="Digite seu nome">
-                </label>
-                
-                <label>
-                <span>SEnha</span>
-                <input type="text" name="" placeholder="Digite uma senha">
-                </label>
-                
-                <input type="submit" value="Cadastrar" class="btn">
-                
-            </div>
-        </form>
-    </div>	
-</div>
-</div>
+            <div class="base-logar">
+                <form action="{{ route('login') }}" method="POST">
+                    @csrf
+                    <div class="col">
+                        <p class="ttl">Logar</p>
+                        <small>Entre com seu login e senha</small>
+
+
+                        <label>
+                            <span>Usuario/ email</span>
+                            <input type="text" name="email" placeholder="Digite seu nome de usuário">
+                        </label>
+                        <label>
+                            <span>Senha</span>
+                            <input type="text" name="password" placeholder="Digite sua senha">	
+                        </label>
+                        <label><a href="#janela1" rel="modal">Esqueci minha senha</a></label>
+                        
+                        <input type="submit" value="entrar" class="btn">
+                    </div>
+                    
+                </form>
+                    
+                    <div class="col2">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <p class="ttl">cadastre-se</p>
+                            <small>Se você ainda não é cadastrado, cadastre-se abaixo</small>
+                            
+                            <label>
+                            <span>email</span>
+                            <input type="text" name="email" placeholder="Digite um email válido">
+                            </label>
+                            
+                            <label>
+                            <span>Nome /usuário</span>
+                            <input type="text" name="name" placeholder="Digite seu nome">
+                            </label>
+                            
+                            <label>
+                            <span>SEnha</span>
+                            <input type="text" name="password" placeholder="Digite uma senha">
+                            </label>
+                            
+                            <label>
+                                <span>Confirmar SEnha</span>
+                                <input type="text" name="password_confirmation" placeholder="Confirme a senha digitada">
+                            </label>
+
+                            <input type="submit" value="Cadastrar" class="btn">
+                        </form>    
+                    </div>
+            </div>	
+        </div>
+    </div>
 
 <!-- modal esqueci senha-->
 <div class="window senha" id="janela1">
