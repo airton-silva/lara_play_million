464a104a5128b7a2fd5d7a7c6208176859c9b2b3
airton-silva <airtonsilvan89@gmail.com>
2024-06-21 15:25:17 -0300

    cria model Question

---
+++ app/Models/Question.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'level_id',
+        'question',
+        'response',
+    ];
+
+    public function level()
+    {
+        return $this->belongsTo(Level::class);
+    }
+}
